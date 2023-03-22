1936d2fedd4112125a56c3c72f82690744f9b222
AlyonaEfros <AlyonaEfros@yandex.ru>
2023-03-22 14:28:09 +0300

    Corrected

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest {
@@ -22,30 +23,28 @@ public class RegistrationTest {
         $("#lastName").setValue("Efros");
         $("#userEmail").setValue("Alyona@F.ru");
         $("#userNumber").setValue("3478585434");
-        $("#genterWrapper").$("[for='gender-radio-3']").click();
+        $("#genterWrapper").$(byText("Other")).click();
         $(".react-datepicker-wrapper").click();
         $(".react-datepicker__month-select").$("[value='5']").click();
         $(".react-datepicker__year-dropdown-container").$("[value='1999']").click();
         $(".react-datepicker__day.react-datepicker__day--029").click();
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbiesWrapper").$("[for='hobbies-checkbox-2']").click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("pictures/cat.jpg");
         $("#currentAddress").setValue("my address");
         $("#state").click();
-        $("#stateCity-wrapper").$("#react-select-3-option-2").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
         $("#city").click();
-        $("#stateCity-wrapper").$("#react-select-4-option-1").click();
+        $("#stateCity-wrapper").$(byText("Panipat")).click();
         $("#submit").click();
 
         $(".modal-content").should(Condition.appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").shouldHave(text("Alyona"));
-        $(".modal-body").shouldHave(text("Alyona@F.ru"));
-        $(".modal-body").shouldHave(text("Other"));
-        $(".modal-body").shouldHave(text("3478585434"));
-        $(".modal-body").shouldHave(text("29 June,1999"));
-        $(".modal-body").shouldHave(text("English"));
-        $(".modal-body").shouldHave(text("Reading"));
+        $(".modal-body").shouldHave(text("Alyona Efros"), text("Alyona@F.ru"), text("Other"),
+                text("3478585434"), text("3478585434"), text("29 June,1999"), text("English"), text("Reading"),
+                text("cat.jpg"), text ("my address"), text("Haryana Panipat"));
+
+
 
     }
 
